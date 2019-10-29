a245d0df8e1d82d700f63ce5b597995cd2f7914b
Mike <2604232849@qq.com>
2019-10-29 16:58:21 +0800

    添加test到本地仓库

---
+++ idea-git-test/src/test1.java
@@ -0,0 +1,13 @@
+public class test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+        System.out.println("Hello Git!!");
+    }
+}
